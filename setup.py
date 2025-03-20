94e730530cddb015dcc3bec39288921a8939f232
amitverma2699 <amit99bhama@gmail.com>
2025-03-20 11:01:05 +0530

    Add files

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="AI_Powered_Task_Management_System",
+    version='0.0.1',
+    author='Amit Verma',
+    author_email="amit99bhama@gmail.com",
+    packages=find_packages()
+)
